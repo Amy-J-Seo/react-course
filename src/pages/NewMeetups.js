740924905c1e56ce8d0ea34202b1d4363b482141
Cbear1234 <amywonderland77@gmail.com>
2021-11-11 22:14:37 +0900

    3:15

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,6 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-    return <div>New Meetups Page
-    </div>
+    const history = useNavigate();
+
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-getting-started-23a83-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            history('/');
+        });
+    }
+
+    return (
+        <section>
+        <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        </section>
+    )
 }
 
 export default NewMeetupsPage;
