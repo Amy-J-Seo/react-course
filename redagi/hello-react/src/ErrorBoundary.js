5e4831d996f257b24fc6ae2117428251dbfaf2d5
Amy-j-seo <amywonderland77@gmail.com>
2022-09-14 17:48:43 +0900

    errorhandling using lifecycle

---
+++ redagi/hello-react/src/ErrorBoundary.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class ErrorBoundary extends Component {
+  state = {
+    error: false,
+  };
+
+  componentDidCatch(error, info) {
+    this.setState({
+      error: true,
+    });
+    console.log({ error, info });
+  }
+
+  render() {
+    if (this.state.error) return <div>Error!</div>;
+    return this.props.children;
+  }
+}
