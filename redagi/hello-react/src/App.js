674b9e4b15d8348415c0811de7f4650ab7a9b349
Amy-j-seo <amywonderland77@gmail.com>
2022-09-14 15:09:59 +0900

    init new prj

---
+++ redagi/hello-react/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  const name = "react";
+
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: "48px",
+    fontWeight: "bold",
+    padding: 16,
+  };
+
+  return (
+    <>
+      <div className="react">{name}</div>
+      <input />
+    </>
+  );
+}
+
+export default App;
