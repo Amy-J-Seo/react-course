c63d21494806521531a42250c320d93963b176ac
Amy-j-seo <amywonderland77@gmail.com>
2022-09-16 10:37:14 +0900

    scss

--- redagi/styling-react/src/App.js
+++ redagi/styling-react/src/App.js
@@ -1,20 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
+import SassComponent from "./SassComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-        <img src={logo} className="logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SassComponent />;
 }
 
 export default App;
