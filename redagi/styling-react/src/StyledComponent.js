b2f6a01ed1b982467be787563440894c646483c8
Amy-j-seo <amywonderland77@gmail.com>
2022-09-16 17:57:10 +0900

    media query with styledcomponent

--- redagi/styling-react/src/StyledComponent.js
+++ redagi/styling-react/src/StyledComponent.js
@@ -1,10 +1,35 @@
 import React from "react";
 import styled, { css } from "styled-components";
 
+//styled-components util function
+const sizes = {
+  desktop: 1024,
+  tablet: 768,
+};
+
+//make media query function according to the size object
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `;
+});
+
 const Box = styled.div`
   background: ${(props) => props.color || "blue"};
   padding: 1rem;
   display: flex;
+  width: 1024px;
+  margin: auto;
+  /* @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  } */
+  ${media.desktop`width:768px;`}
+  ${media.tablet`width:100%;`}
 `;
 
 const Button = styled.button`
