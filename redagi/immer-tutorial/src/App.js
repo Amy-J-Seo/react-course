bc9a7d9a30987c1616727c22a1271cd9a3168f2b
Amy-j-seo <amywonderland77@gmail.com>
2022-09-20 11:07:59 +0900

    use immer

--- redagi/immer-tutorial/src/App.js
+++ redagi/immer-tutorial/src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useCallback, useState } from "react";
+import "./App.css";
+import produce from "immer";
 
 function App() {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: "", username: "" });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
+
+  //input edit function
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setForm(
+        produce(form, (draft) => {
+          draft[name] = value;
+        })
+      );
+    },
+    [form]
+  );
+
+  //form register function
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+
+      //add new item to the array
+      setData(
+        produce(data, (draft) => {
+          draft.array.push(info);
+        })
+      );
+      //reset the form
+      setForm({
+        name: "",
+        username: "",
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.username]
+  );
+
+  //delete item function
+  const onRemove = useCallback(
+    (id) => {
+      setData(
+        produce(data, (draft) => {
+          draft.array.splice(
+            draft.array.findIndex((info) => info.id === id),
+            1
+          );
+        })
+      );
+    },
+    [data]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="username"
+          placeholder="아이디"
+          value={form.username}
+          onChange={onChange}
+        />
+        <input
+          name="name"
+          placeholder="이름"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
