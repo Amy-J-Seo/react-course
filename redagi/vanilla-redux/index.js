56c2801b609241de6342162bb1db011165232930
Amy-j-seo <amywonderland77@gmail.com>
2022-12-01 17:57:16 +0900

    redux installed

--- redagi/vanilla-redux/index.js
+++ redagi/vanilla-redux/index.js
@@ -1,1 +1,20 @@
-console.log("parcel");
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+// 액션 이름
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+//엑션 타입 설정
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = () => ({ type: INCREASE });
+const decrease = () => ({ type: DECREASE });
+
+//초기값 설정
+const initalState = {
+  toggle: false,
+  counter: 0,
+};
