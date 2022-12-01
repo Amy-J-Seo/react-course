95447e5ab84acf435c1d1c82641ee1f2f8e9aad5
Amy-j-seo <amywonderland77@gmail.com>
2022-12-01 17:24:39 +0900

    static contextType

--- redagi/context-tutorial/src/components/ColorBox.js
+++ redagi/context-tutorial/src/components/ColorBox.js
@@ -1,28 +1,25 @@
-import React from "react";
-import { ColorConsumer } from "../context/color";
+import React, { useContext } from "react";
+import ColorContext from "../context/color";
 
 function ColorBox() {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          ></div>
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor,
-            }}
-          ></div>
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      ></div>
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      ></div>
+    </>
   );
 }
 
--- redagi/context-tutorial/src/components/SelectColors.js
+++ redagi/context-tutorial/src/components/SelectColors.js
@@ -1,49 +1,85 @@
-import React from "react";
+import React, { Component } from "react";
 import { ColorConsumer } from "../context/color";
-
+import ColorContext from "../context/color";
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-function SelectColors() {
-  return (
-    <div>
-      <h2>Select colors</h2>
-      {/* <div style={{ display: "flex" }}>
-        {colors.map((color) => (
-          <div
-            key={color}
-            style={{
-              background: color,
-              width: "24px",
-              height: "24px",
-              cursor: "pointer",
-            }}
-          />
-        ))}
-      </div> */}
-      <ColorConsumer>
-        {({ actions }) => (
-          <div style={{ display: "flex" }}>
-            {colors.map((color) => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: "24px",
-                  height: "24px",
-                  cursor: "pointer",
-                }}
-                onClick={() => actions.setColor(color)}
-                onContextMenu={(e) => {
-                  e.preventDefault();
-                  actions.setSubcolor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </ColorConsumer>
-      <hr />
-    </div>
-  );
+// function SelectColors() {
+//   return (
+//     <div>
+//       <h2>Select colors</h2>
+//       {/* <div style={{ display: "flex" }}>
+//         {colors.map((color) => (
+//           <div
+//             key={color}
+//             style={{
+//               background: color,
+//               width: "24px",
+//               height: "24px",
+//               cursor: "pointer",
+//             }}
+//           />
+//         ))}
+//       </div> */}
+//       <ColorConsumer>
+//         {({ actions }) => (
+//           <div style={{ display: "flex" }}>
+//             {colors.map((color) => (
+//               <div
+//                 key={color}
+//                 style={{
+//                   background: color,
+//                   width: "24px",
+//                   height: "24px",
+//                   cursor: "pointer",
+//                 }}
+//                 onClick={() => actions.setColor(color)}
+//                 onContextMenu={(e) => {
+//                   e.preventDefault();
+//                   actions.setSubcolor(color);
+//                 }}
+//               />
+//             ))}
+//           </div>
+//         )}
+//       </ColorConsumer>
+//       <hr />
+//     </div>
+//   );
+// }
+
+class SelectColors extends Component {
+  static contextType = ColorContext;
+  handleSetColor = (color) => {
+    this.context.actions.setColor(color);
+  };
+  handleSetSubColor = (subColor) => {
+    this.context.actions.setSubcolor(subColor);
+  };
+  render() {
+    return (
+      <div>
+        <h2>Choose the color</h2>
+        <div style={{ display: "flex" }}>
+          {colors.map((color) => (
+            <div
+              key={color}
+              style={{
+                backgroundColor: color,
+                width: "24px",
+                height: "24px",
+                cursor: "pointer",
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                this.handleSetSubColor(color);
+              }}
+            />
+          ))}
+        </div>
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default SelectColors;
